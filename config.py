b1e02433e0cb01042e9c071a40fedf814245c47d
Yuwei Yan <yuweiyan@uchicago.edu>
2024-09-13 18:12:02 +0800

    use abs fp

--- config.py
+++ config.py
@@ -1,3 +1,14 @@
+import os
+
+# Get the absolute path to the project's root directory
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Define data directories
+RAW_DATA_DIR = os.path.join(BASE_DIR, 'data', 'raw_data')
+ANALYSIS_DATA_DIR = os.path.join(BASE_DIR, 'data', 'analysis_data')
+RISKMACRO_DATA_DIR = os.path.join(BASE_DIR, 'data', 'riskmacro_data')
+INDEX_DATA_DIR = os.path.join(BASE_DIR, 'data', 'index_components')
+
 base_url = "https://optioncharts.io/options"
 cpus = 10
 tries = 3
